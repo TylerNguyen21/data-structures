98f2dc2c80828f73363ee399871e4a45e5deeb7f
TylerNguyen21 <tydnguyen@ucdavis.edu>
2018-11-07 21:03:30 -0800

    finished linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,38 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var newNode = Node(value);
+    if (list.head === null) {
+      list.head = newNode;
+      list.tail = newNode;
+    } else {
+      var oldTail = list.tail;
+      list.tail = newNode;
+      oldTail.next = newNode;
+    }
+    
   };
 
   list.removeHead = function() {
+    var removedHead = list.head;
+    list.head = list.head.next;
+    return removedHead.value;
   };
 
   list.contains = function(target) {
+    //if there is a next node
+    if (list.head.value === target) {
+      return true;
+    }
+    var current = list.head;
+    
+    while (current.next !== null) {
+      if (current.next.value === target) {
+        return true;
+      }
+      current = current.next;  
+    }
+    return false;
   };
 
   return list;
