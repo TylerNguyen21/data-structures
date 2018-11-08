7f01ebdcce44f40d7ea04e7db3a0cd07985c27f7
TylerNguyen21 <tydnguyen@ucdavis.edu>
2018-11-07 22:00:37 -0800

    finished tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,17 +3,40 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = [];  // fix me
+  _.extend(newTree, treeMethods);
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  // make a new tree
+  var childTree = Tree(value);
+  //add the child to current children array
+  this.children.push(childTree);
 };
 
 treeMethods.contains = function(target) {
+  //hold current tree
+  console.log(this);
+  var answer = false;
+  if (this.value === target ) {
+    answer = true;
+  }
+  
+  if (this.children.length > 0 ) {
+  
+    for ( var i = 0; i < this.children.length; i++ ) {
+      
+      if (treeMethods.contains.call( this.children[i], target)) {
+        return true;
+      }
+      
+    } 
+  }
+  
+  return answer;
 };
 
 
