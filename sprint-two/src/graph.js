d420563e90a40013ed0a2f65cc64420b9751f971
TylerNguyen21 <tydnguyen@ucdavis.edu>
2018-11-08 21:26:55 -0800

    finished graph!

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -7,18 +7,22 @@ var Graph = function() {
 
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
-  this.nodes[node] = []; 
+  this.nodes[JSON.stringify(node)] = []; 
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
-  return Object.keys(this.nodes).includes(node.toString());
+  return Object.keys(this.nodes).includes(JSON.stringify(node));
 };
 
 // Removes a node from the graph.
-Graph.prototype.removeNode = function(node) {
-  delete this.nodes[node];
-    
+Graph.prototype.removeNode = function(node) { 
+  for (var key in this.nodes) {
+    this.nodes[key] = this.nodes[key].filter(function (value) {
+      return value !== node;
+    });
+  }
+  delete this.nodes[JSON.stringify(node)];
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
@@ -29,17 +33,25 @@ Graph.prototype.hasEdge = function(fromNode, toNode) {
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
   this.nodes[fromNode].push(toNode);
+  this.nodes[toNode].push(fromNode);
 };
 
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
-  this.nodes[fromNode].filter(function(value) {
-    return value !== toNode.toString();
+  this.nodes[fromNode] = this.nodes[fromNode].filter(function(value) {
+    return value !== toNode;
+  });
+  this.nodes[toNode] = this.nodes[toNode].filter(function(value) {
+    return value !== fromNode;
   });
 };
 
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
+  for( var key in this.nodes) {
+    cb(key);
+  }
+
 };
 
 /*
