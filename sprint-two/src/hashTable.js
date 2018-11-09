16ded5b355c6a923ab142ecb01e43dcb8b73ed55
TylerNguyen21 <tydnguyen@ucdavis.edu>
2018-11-09 11:24:11 -0800

    finished bare min for hashtable

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -3,18 +3,58 @@
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+  //complexity = constant O(1)
 };
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  if (Array.isArray(this._storage[index])) {
+    var oldIndex = this._storage[index].findIndex(function(pair) {
+      return pair[0] === k; 
+    });
+    if (oldIndex >= 0) {
+      this._storage[index][oldIndex].splice(1, 1, v);
+    } else if (oldIndex === -1) {
+      this._storage[index].push([k, v]);
+    }
+
+  } else {
+    this._storage[index] = [[k, v]]; 
+  }
+  //constant if no collision else linear
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  if (Array.isArray(this._storage[index])) {
+    var oldIndex = this._storage[index].findIndex(function(pair) {
+      return pair[0] === k; 
+    });
+    if (oldIndex >= 0) {
+      return this._storage[index][oldIndex][1];
+    } else if (oldIndex === -1) {
+      return undefined;
+    }
+  }
+  //linear 
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  if (Array.isArray(this._storage[index])) {
+  // there is just one tuple 
+    if (this._storage[index].length === 1) {
+      this._storage[index] = undefined;
+    } else if ( this._storage[index].length > 1) {
+      var oldIndex = this._storage[index].findIndex(function(pair) {
+        return pair[0] === k; 
+      });
+      if (oldIndex >= 0) {
+        delete this._storage[index][oldIndex];
+      }  
+    }
+  }
+  //constant unless collision then its linear
 };
 
 
