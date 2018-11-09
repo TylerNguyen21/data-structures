b0db80812832f3cba5179a0a4c67f7e2a895a9f1
TylerNguyen21 <tydnguyen@ucdavis.edu>
2018-11-08 21:32:35 -0800

    added complexity to graph

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -3,16 +3,19 @@
 // Instantiate a new graph
 var Graph = function() {
   this.nodes = {};
+  //complexity = constant O(1)
 };
 
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
-  this.nodes[JSON.stringify(node)] = []; 
+  this.nodes[JSON.stringify(node)] = [];
+  //complexity = constant O(1) 
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
   return Object.keys(this.nodes).includes(JSON.stringify(node));
+  //complexity = quadratic O(n^2);
 };
 
 // Removes a node from the graph.
@@ -23,17 +26,20 @@ Graph.prototype.removeNode = function(node) {
     });
   }
   delete this.nodes[JSON.stringify(node)];
+  //complexity quadratic O(n^2)
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
   return this.nodes[fromNode].includes(toNode);
+  //complexity linear O(n)
 };
 
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
   this.nodes[fromNode].push(toNode);
   this.nodes[toNode].push(fromNode);
+  //complexity constant O(1)
 };
 
 // Remove an edge between any two specified (by value) nodes.
@@ -44,14 +50,15 @@ Graph.prototype.removeEdge = function(fromNode, toNode) {
   this.nodes[toNode] = this.nodes[toNode].filter(function(value) {
     return value !== fromNode;
   });
+  //complexity linear O(n)
 };
 
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
-  for( var key in this.nodes) {
+  for ( var key in this.nodes) {
     cb(key);
   }
-
+  //complexity linear O(n);
 };
 
 /*
